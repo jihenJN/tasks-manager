41ca4892638b411bc4cb09fc022d97eb74f1f984
jihenJN <jihenjihen23@gmail.com>
2025-11-11 11:24:41 +0100

    feat : create components and styling

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { TaskForm } from './components/task-form/task-form';
+import { TaskList } from './components/task-list/task-list';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [TaskForm,TaskList],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
