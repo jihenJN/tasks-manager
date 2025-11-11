ff6ac4d0bab73da7ebbf97f75aa1780a2cebda69
jihenJN <jihenjihen23@gmail.com>
2025-11-11 11:56:13 +0100

    feat : taskform as reactive form

--- src/app/components/task-form/task-form.ts
+++ src/app/components/task-form/task-form.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
-
+import { FormControl, FormControlName, FormGroup, ReactiveFormsModule } from '@angular/forms';
 @Component({
   selector: 'app-task-form',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './task-form.html',
   styleUrl: './task-form.css',
 })
 export class TaskForm {
+  taskForm = new FormGroup({
+    task: new FormControl('', { nonNullable: true }),
+    status: new FormControl('todo', { nonNullable: true }),
+  });
 
+  onSubmit(){
+    console.log(this.taskForm.getRawValue())
+  }
 }
